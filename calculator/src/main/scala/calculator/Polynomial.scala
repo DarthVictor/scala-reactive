99b89b2d47198080fc774d52bd0415ccd81d832e
Victor <follet.victor@gmail.com>
2015-04-21 12:08:10 +0300

    Polynomial.scala is done.

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,28 @@
 package calculator
 
+case class ZeroCoaficientException(smth:String)  extends Exception(smth)
+
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    if (a() == 0) {
+      throw new ZeroCoaficientException("Not square polinom")
+    }
+    else{
+      Signal(b() * b() - 4 * a() * c())
+    }
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    if (a() == 0) {
+      throw new ZeroCoaficientException("Not square polinom")
+    }
+    else if (delta() == 0){
+      Signal(Set(-b()/(2*a())))
+    }
+    else {
+      Signal(Set( (-b() - delta())/(2*a()), (-b() + delta())/(2*a())))
+    }
   }
 }
