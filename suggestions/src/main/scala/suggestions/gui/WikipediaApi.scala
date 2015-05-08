de38d5332370e5a4e47b05cbece078d2bf48ab0b
Victor <follet.victor@gmail.com>
2015-05-08 13:36:10 +0300

    SwingApi and WikipediaApi passed tests

--- suggestions/src/main/scala/suggestions/gui/WikipediaApi.scala
+++ suggestions/src/main/scala/suggestions/gui/WikipediaApi.scala
@@ -75,7 +75,7 @@ trait WikipediaApi {
      * Note: uses the existing combinators on observables.
      */
     def timedOut(totalSec: Long): Observable[T] = obs.takeUntil(
-        Observable.Interval(totalSec second).take(1)
+        Observable.interval(totalSec second).take(1)
     )
     /** Given a stream of events `obs` and a method `requestMethod` to map a request `T` into
      * a stream of responses `S`, returns a stream of all the responses wrapped into a `Try`.
@@ -102,8 +102,7 @@ trait WikipediaApi {
      *
      * Observable(Success(1), Succeess(1), Succeess(1), Succeess(2), Succeess(2), Succeess(2), Succeess(3), Succeess(3), Succeess(3))
      */
-    def concatRecovered[S](requestMethod: T => Observable[S]): Observable[Try[S]] = obs.flatMap(t)
-
+    def concatRecovered[S](requestMethod: T => Observable[S]): Observable[Try[S]] = obs.map(t => requestMethod(t).recovered).concat
   }
 
 }
